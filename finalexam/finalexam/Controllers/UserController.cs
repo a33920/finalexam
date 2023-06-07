3b016cdaffba252a14cf80c1c4b9130f9a9701dc
a33920 <a33920vic@gmail.com>
2023-06-07 20:05:36 +0800

    final

---
+++ finalexam/finalexam/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace finalexam.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Music(string name,int? number)
+        {
+            var result = "";
+            if(number == 1)
+            {
+                result = "do";
+            }
+            if (number == 2)
+            {
+                result = "re";
+            }
+            if (number == 3)
+            {
+                result = "mi";
+            }
+            if (number == 4)
+            {
+                result = "fa";
+            }
+            if (number == 5)
+            {
+                result = "so";
+            }
+            if (number == 6)
+            {
+                result = "la";
+            }
+            if (number == 7)
+            {
+                result = "xi";
+            }
+            if (number != 1 && number != 2 && number != 3 && number != 4 && number != 5
+                && number != 6 && number != 7)
+            {
+                result = "錯誤";
+            }
+            ViewBag.Name = name;
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
